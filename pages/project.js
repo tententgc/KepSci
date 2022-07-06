e2ea4054a27369a0a7da1e497fc7a265ad1a73db
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-07-06 20:14:16 +0700

    🔥 Remove fetch API function

--- pages/project.js
+++ pages/project.js
@@ -3,44 +3,7 @@ import Image from 'next/image'
 import styles from  '../styles/Project.module.css' 
 import { useState, useEffect } from 'react'
 import CustomTable from '../components/Customtable'
-// function Project() {
 
-//     const [showPosts, setshowPosts] = useState()
-//     const apiUrl = 'https://jsonplaceholder.typicode.com/todos/'
-//     let displayData
-
-//     async function pullJson() {
-//         const response = await fetch(apiUrl)
-//         const responseData = await response.json()
-//         displayData = responseData.map(function (todo) {
-//             return (
-//                 <>
-//                 <p key={todo.id}>{todo.title} </p>
-//                 </>
-//             )
-//         })
-//         setshowPosts(displayData)
-//     }
-
-//     useEffect(() => {
-//         pullJson()
-//     }, [])
-
-//     return (
-        // <div className={styles.container
-        // }>
-        //     <Head>
-        //         <title>Project |  KepSci</title>
-        //         <link rel='icon' href='/favicon.ico' />
-        //     </Head>
-        //     <main className={styles.main}>
-        //         <h1>Project List</h1>
-        //         {showPosts}
-        //     </main>
-
-//         </div>
-//     )
-// }
 const Project = () => {
     return (
     <div className={styles.container
