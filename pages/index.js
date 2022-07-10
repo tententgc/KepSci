f55d7ec9568068ee518e99e3ae03da538544f56f
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-07-10 17:54:41 +0700

    Test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main>
       <div className="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
-          <p className="text-white font-extrabold text-3xl md:text-5xl">   Ghostwind CSS
+          <p className="text-black font-extrabold text-3xl md:text-5xl">   Ghostwind CSS
           </p>
         <p className="text-xl md:text-2xl text-gray-500"> Welcome to my Blog </p>
         </div>
