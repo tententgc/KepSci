8f3548e19ab3a56c5b845e854ca986257c11e3c7
First1zaza <69394299+First1zaza@users.noreply.github.com>
2022-07-17 16:49:50 +0700

    Nothing

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className='banner h-72 flex justify-center items-center'>
+      <div className='banner h-64 flex justify-center items-center'>
         <p className='text-center text-5xl font-semibold mb-5 text-sky-800'>Research Libraries</p>
+      </div>
+      <div className='main-2'>
+
       </div>
     </>
   )
