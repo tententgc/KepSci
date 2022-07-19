56b486bcbdcce107f41ffcadeb67b1d1585c24fb
First1zaza <69394299+First1zaza@users.noreply.github.com>
2022-07-19 15:59:49 +0700

    Edit Home Page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Layout from '/components/layouts'
 export default function Home() {
   return (
     <Layout>
-      <p className='text-center mt-10 text-5xl font-semibold mb-5'>Research Libraries</p>
+      <p className='text-center mt-10 mb-0 text-5xl font-semibold mb-5'>Research Libraries</p>
+      <hr className='max-w-xl mx-auto my-3 bg-gray-400 h-0.5'></hr>
     </Layout>
   )
 }
