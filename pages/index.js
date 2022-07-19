1a497ee4703faad8eef864fced7760e92aadd3af
First1zaza <69394299+First1zaza@users.noreply.github.com>
2022-07-19 23:28:47 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,14 @@ import Layout from '/components/layouts'
 export default function Home() {
   return (
     <Layout>
-      <p className='text-center mt-10 mb-0 text-5xl font-semibold mb-5'>Research Libraries</p>
-      <hr className='max-w-xl mx-auto my-3 bg-gray-400 h-0.5'></hr>
+      <div className='section'>
+        <p className='text-center mt-10 mb-3 text-5xl font-semibold'>Research Libraries</p>
+        <p className='text-center text-gray-500	'>Public research - King Mongkut's University of Technology Thonburi</p>
+        <hr className='max-w-2xl mx-auto my-3 bg-gray-400 h-0.5'></hr>
+      </div>
+      <div className='section'>
+        
+      </div>
     </Layout>
   )
 }
