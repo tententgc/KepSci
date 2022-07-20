296e2cfb205cebb2cb97ac4a8e8e5079f41bf820
First1zaza <69394299+First1zaza@users.noreply.github.com>
2022-07-20 17:57:52 +0700

    Edit Home and other page

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,22 @@ import localImage from "../images/logo.png";
 export default function Home() {
   return (
     <Layout>
-      <div className='section mb-10'>
+      <div className='section mb-10 flex justify-center'>
         <Image
           src={localImage}
-          alt="Picture of the author"
-          className='w-20'
+          className='mx-auto'
+          width={750}
+          height={250}
         />
+      </div>
+      <div className='section mb-10'>
         <p className='text-center mt-14 mb-3 text-5xl font-semibold'>Research Libraries</p>
         <p className='text-center text-gray-500	'>Public journal and articles - King Mongkut's University of Technology Thonburi</p>
         {/* <hr className='max-w-2xl sm:mx-5 md:mx-auto my-3 bg-gray-400 h-0.5'></hr> */}
       </div>
       <div className='section flex justify-center'>
         <Link href="/project">
-          <button type="button" className='mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Search Documentation</button>
+          <button type="button" className='mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Search journal or articles</button>
         </Link>
       </div>
     </Layout>
