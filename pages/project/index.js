4dede7062cd22015c909f992355ad1753096b3ee
First1zaza <69394299+First1zaza@users.noreply.github.com>
2022-07-21 14:30:03 +0700

    edit
    - view page
    - search page

--- pages/project/index.js
+++ pages/project/index.js
@@ -4,22 +4,29 @@ import Layout from '/components/layouts'
 export default function Project() {
   return (
     <Layout>
-      <div className='max-w-screen-md mx-auto'>
-        <div className='section mb-14'>
+      <div className='max-w-screen-lg mx-auto'>
+        <div className='section mb-9'>
           <p className=''>Looking for something?</p>
           <input type="text" className="mt-1 block w-full px-4 py-2 bg-white border border-slate-300 rounded-md text-sm rounded-2xl" placeholder='Search'/>
         </div>
         <div className='section'>
-          <div class="grid grid-cols-5 gap-4">
-            <div class="">
-              
-            </div>
-            <div class="col-span-4">
-              <Link href="/">
-                <a className='text-blue-700 font-semibold text-lg hover:text-blue-600'>Header</a>
-              </Link>
-              <p className='text-gray-500'>Detail</p>
-            </div>
+          <div className='card p-3 mb-3 border-b-2 border-b-gray-300'>
+            <Link href="/project/0972">
+              <a className='text-blue-700 font-semibold text-lg hover:text-blue-500'>Coronavirus Drug Discovery</a>
+            </Link>
+            <p className='text-gray-500'>Coronavirus Drug Discovery: SARS-CoV-2 (Covid 19) Prevention, Diagnosis, and Treatment presents comprehensive information on antiviral drug discovery from medicinal plants, animals ... </p>
+          </div>
+          <div className='card p-3 mb-3 border-b-2 border-b-gray-300'>
+            <Link href="/project/0973">
+              <a className='text-blue-700 font-semibold text-lg hover:text-blue-500'>Coronavirus Drug Discovery</a>
+            </Link>
+            <p className='text-gray-500'>Coronavirus Drug Discovery: SARS-CoV-2 (Covid 19) Prevention, Diagnosis, and Treatment presents comprehensive information on antiviral drug discovery from medicinal plants, animals ... </p>
+          </div>
+          <div className='card p-3 mb-3 border-b-2 border-b-gray-300'>
+            <Link href="/project/0974">
+              <a className='text-blue-700 font-semibold text-lg hover:text-blue-500'>Coronavirus Drug Discovery</a>
+            </Link>
+            <p className='text-gray-500'>Coronavirus Drug Discovery: SARS-CoV-2 (Covid 19) Prevention, Diagnosis, and Treatment presents comprehensive information on antiviral drug discovery from medicinal plants, animals ... </p>
           </div>
         </div>
       </div>
