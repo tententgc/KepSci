d7a00a02c3b3e104152b6679a9e7182c858c51e2
First1zaza <69394299+First1zaza@users.noreply.github.com>
2022-07-28 00:46:57 +0700

    edit layout

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,13 @@ export default function Home() {
         />
       </div>
       <div className='section mb-10'>
-        <p className='text-center mt-14 mb-3 text-5xl font-semibold'>Research Libraries</p>
-        <p className='text-center text-gray-500	'>Public journal and articles - King Mongkut's University of Technology Thonburi</p>
+        <p className='text-center mt-14 mb-3 text-3xl md:text-4xl font-semibold'>ฐานข้อมูลวิทยานิพนธ์</p>
+        <p className='text-center text-lg text-gray-500'>มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี</p>
         {/* <hr className='max-w-2xl sm:mx-5 md:mx-auto my-3 bg-gray-400 h-0.5'></hr> */}
       </div>
       <div className='section flex justify-center'>
         <Link href="/project">
-          <button type="button" className='mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Search journal or articles</button>
+          <button type="button" className='mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>ค้นหาวิทยานิพนธ์</button>
         </Link>
       </div>
     </Layout>
