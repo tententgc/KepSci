9450df394a666d71bd9bacf1470d38fab03036e0
First1zaza <69394299+First1zaza@users.noreply.github.com>
2022-07-27 17:02:09 +0700

    Update index.js

--- pages/project/index.js
+++ pages/project/index.js
@@ -4,13 +4,13 @@ import Layout from '/components/layouts'
 export default function Project() {
   return (
     <Layout>
-      <div className='max-w-screen-lg mx-auto'>
-        <div className='card bg-gray-300 mb-5 p-4 rounded-md border'>
-          <div className='input-group mb-3'>
-            <span className="block font-medium text-slate-700">Title or Header</span>
-            <input type="text"  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm"/>
-          </div>
+      <div className='card bg-gray-200 mb-5 p-4 rounded-md border'>
+        <div className='input-group mb-3'>
+          <span className="block font-medium text-slate-700">Title or Header</span>
+          <input type="text"  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm"/>
         </div>
+      </div>
+      <div className='max-w-screen-lg mx-auto'>
         <div className='section'>
           <div className='card p-3 mb-3 border-b-2 border-b-gray-300'>
             <Link href="/project/0972">
