ab65d0694df56bb47b1561624f992d14b082288e
First1zaza <69394299+First1zaza@users.noreply.github.com>
2022-07-27 14:42:43 +0700

    Update index.js

--- pages/project/index.js
+++ pages/project/index.js
@@ -5,9 +5,11 @@ export default function Project() {
   return (
     <Layout>
       <div className='max-w-screen-lg mx-auto'>
-        <div className='section mb-9'>
-          <p className=''>Looking for something?</p>
-          <input type="text" className="mt-1 block w-full px-4 py-2 bg-white border border-slate-300 rounded-md text-sm rounded-2xl" placeholder='Search'/>
+        <div className='card bg-gray-300 mb-5 p-4 rounded-md border'>
+          <div className='input-group mb-3'>
+            <span className="block font-medium text-slate-700">Title or Header</span>
+            <input type="text"  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm"/>
+          </div>
         </div>
         <div className='section'>
           <div className='card p-3 mb-3 border-b-2 border-b-gray-300'>
