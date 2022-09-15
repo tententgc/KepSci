ebe9869c95fb69ef3325b4b2eddde7ed95ba4184
First1zaza <69394299+First1zaza@users.noreply.github.com>
2022-09-15 16:47:57 +0700

    Edit Navbar

--- components/nav.js
+++ components/nav.js
@@ -1,26 +1,34 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import Link from 'next/link'
+import {useState} from 'react';
 
 export default function Navbar() {
+    const [isActive, setIsActive] = useState(false);
+
+    const active = event => {
+        setIsActive(current => !current);
+    }
     return (
-        <nav className="navbar bg-gray-600 shadow shadow-black">
-            <div className="container py-3 mx-auto sm:flex">
-                <button className="navbar-toggler sm:hidden rounded-md px-3 py-1 border border-solid mb-2" type="button">
+        <nav className="navbar bg-white">
+            <div className="container py-3 mx-auto sm:flex md:px-5">
+                <button className="navbar-toggler sm:hidden rounded-md px-3 py-1 border border-solid" type="button" onClick={active}>
                     <FontAwesomeIcon icon={faBars} size="lg"/>
                 </button>
-                <ul className="navbar-nav sm:flex ml-auto">
-                    <li className="nav-item">
-                        <Link href="/">
-                            <a className="nav-link">หน้าแรก</a>
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link href="/login">
-                            <a className="nav-link">เข้าสู่ระบบ</a>
-                        </Link>
-                    </li>
-                </ul>
+                <div className="ml-auto">
+                    <ul className={"navbar-nav sm:flex" + (isActive ?  "flex-col" : " hidden")}>
+                        <li className="nav-item">
+                            <Link href="/">
+                                <a className="nav-link">หน้าแรก</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/login">
+                                <a className="nav-link">เข้าสู่ระบบ</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
