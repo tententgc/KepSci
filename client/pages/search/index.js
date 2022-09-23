15680a68221ebc1269a12ecab8910ddb063fd1f8
First1zaza <69394299+First1zaza@users.noreply.github.com>
2022-09-23 22:12:43 +0700

    Design search page & research page

--- client/pages/search/index.js
+++ client/pages/search/index.js
@@ -1,22 +1,19 @@
 import Layout from '/components/layouts'
 import Link from 'next/link'
-import sheet from '/images/sheet.png'
-import Image from 'next/image'
 
 export default function Home() {
     return (
         <Layout>
-            <div className='mx-auto max-w-5xl'>
-                <div className='bg-white rounded-lg p-5 mb-5 drop-shadow-md'>
+                <div className='bg-white rounded-lg p-5 mb-5 drop-shadow-md mx-auto max-w-6xl'>
                     <p className="text-lg">ค้นหางานวิจัยและวิทยานิพนธ์</p>
                     <hr className="mb-3 h-px bg-black border-0"></hr>
                     <div className='input-group mb-3'>
-                        <span className="block font-medium text-slate-700">ชื่องานวิจัยหรือวิทยานิพนธ์</span>
+                        <span className="block font-medium text-slate-700">ชื่องานวิจัยหรือวิทยานิพนธ์ หรือคำที่เกี่ยวข้อง</span>
                         <input type="text"  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm"/>
                     </div>
                     <div class="grid grid-cols-1 sm:grid-cols-2 sm:gap-x-4">
                         <div className='input-group mb-3'>
-                            <span className="block font-medium text-slate-700">ปีการศึกษา</span>
+                            <span className="block font-medium text-slate-700">ชื่อคนเขียน</span>
                             <input type="text"  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm"/>
                         </div>
                         <div className='input-group mb-3'>
@@ -25,15 +22,53 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className='bg-white rounded-lg p-5 drop-shadow-md'>
-                    <Link href="/research/">
-                        <a className=''>
-                            ชื่องานวิจัยหรือวิทยานิพนธ์
-                        </a>
-                    </Link>
-                    <p></p>
+                <div className='bg-white rounded-lg p-7 drop-shadow-md mx-auto max-w-6xl'>
+                    <div className='section mb-7'>
+                        <Link href="/research">
+                            <a className='text-blue-500 hover:text-blue-700'>
+                                ชื่องานวิจัยหรือวิทยานิพนธ์
+                            </a>
+                        </Link>
+                        <p className='text-gray-400'>รายละเอียดโดยสังเขป</p>
+                        <p className='text-gray-400'>จัดทำโดย : นายโอ เค </p>
+                    </div>
+                    <div className='section mb-7'>
+                        <Link href="/research">
+                            <a className='text-blue-500 hover:text-blue-700'>
+                                ชื่องานวิจัยหรือวิทยานิพนธ์
+                            </a>
+                        </Link>
+                        <p className='text-gray-400'>รายละเอียดโดยสังเขป</p>
+                        <p className='text-gray-400'>จัดทำโดย : นายโอ เค </p>
+                    </div>
+                    <div className='section mb-7'>
+                        <Link href="/research">
+                            <a className='text-blue-500 hover:text-blue-700'>
+                                ชื่องานวิจัยหรือวิทยานิพนธ์
+                            </a>
+                        </Link>
+                        <p className='text-gray-400'>รายละเอียดโดยสังเขป</p>
+                        <p className='text-gray-400'>จัดทำโดย : นายโอ เค </p>
+                    </div>
+                    <div className='section mb-7'>
+                        <Link href="/research">
+                            <a className='text-blue-500 hover:text-blue-700'>
+                                ชื่องานวิจัยหรือวิทยานิพนธ์
+                            </a>
+                        </Link>
+                        <p className='text-gray-400'>รายละเอียดโดยสังเขป</p>
+                        <p className='text-gray-400'>จัดทำโดย : นายโอ เค </p>
+                    </div>
+                    <div className='section'>
+                        <Link href="/research">
+                            <a className='text-blue-500 hover:text-blue-700'>
+                                ชื่องานวิจัยหรือวิทยานิพนธ์
+                            </a>
+                        </Link>
+                        <p className='text-gray-400'>รายละเอียดโดยสังเขป</p>
+                        <p className='text-gray-400'>จัดทำโดย : นายโอ เค </p>
+                    </div>
                 </div>
-            </div>
         </Layout>
     )
 
